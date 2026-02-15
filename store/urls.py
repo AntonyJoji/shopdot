a37fb838afdd30da97b08b7706a39836c8faf914
AntonyJoji <antonyjoji.antony@gmail.com>
2026-02-15 21:00:28 +0530

    cart delete function added

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import *
-urlpatterns = [
-    path('',landing_page),
-    path('storehome/',store_home),
-    path('productpage/',product_page),
-    path('details/<str:key>/',product_details),
-    path('addtocart/',addtocart),
-    path('cart/',shopping_cart),
 
+urlpatterns = [
+    path('', landing_page, name='landing_page'),
+    path('storehome/', store_home, name='store_home'),
+    path('productpage/', product_page, name='product_page'),
+    path('details/<str:key>/', product_details, name='product_details'),
+    path('addtocart/', addtocart, name='addtocart'),
+    path('cart/', shopping_cart, name='shopping_cart'),  
+    path('deletecart/<str:key>/', delete_cart_item, name='delete_cart_item'), 
 ]
--- store/views.py
+++ store/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import productdata 
 
-def landing_page(req):
-    return render(req, 'landingpage.html')
+def landing_page(request):
+    return render(request, "landingpage.html", {"is_home": True})
+
 
 def store_home(req):
     page=loader.get_template('homepage.html')
@@ -64,3 +65,27 @@ def shopping_cart(req):
                     pass 
     response = page.render({'cart_items': cart_items, 'grand_total': grand_total}, req)
     return HttpResponse(response)
+
+
+from django.shortcuts import redirect
+
+def delete_cart_item(req, key):
+    cookie_data = req.COOKIES.get('pid')
+
+    if cookie_data:
+        items = cookie_data.split(',')
+        new_items = []
+
+        for i in items:
+            if ':' in i:
+                proid, qty = i.split(':')
+                if proid != key:
+                    new_items.append(i)
+
+        new_cookie_data = ','.join(new_items)
+
+        response = redirect('shopping_cart')
+        response.set_cookie('pid', new_cookie_data)
+        return response
+
+    return redirect('shopping_cart')
